1613873ed557664a800073b29058594d71e761b4
Andrius Martinkėnas <andrius.martinkenas321@gmail.com>
2022-08-04 14:28:53 +0300

    014

--- 014/index.php
+++ 014/index.php
@@ -30,6 +30,7 @@ for($i = 0; $i < 5; $i++){
     echo "dabar: $i \n";
 }
 */
+/*
 $colors = ['red', 'green', 'blue', 'yellow'];
 foreach ($colors as &$value) {
 
@@ -39,3 +40,42 @@ foreach ($colors as &$value) {
 
 }
 print_r($colors);
+*/
+
+
+$colors = [
+    ['red', 'green', 'blue', 'yellow'],
+    'labas',
+    ['drambys', 'bebras', 'briedis', 'barsukas','traktorius'],
+    ['77', '12']
+];
+
+echo $colors[1][0];
+
+foreach($colors as $stalcius){
+    if(is_array($stalcius)){
+        foreach($stalcius as $daiktas){
+            echo "$daiktas\n";
+        }
+    }else {
+            echo "$stalcius\n";
+    }
+}
+
+$_3x3 =[];
+$count = 0;
+/*
+foreach(range(1,3) as $_){
+    $_3 = [];
+        foreach(range(1,3) as $_){
+            $_3[] = ++$count;
+        }
+    $_3x3[] = $_3;
+}*/
+
+foreach(range(0,2) as $a){
+        foreach(range(0,2) as $b){
+            $_3x3[$a][$b] = ++$count;
+        }
+}
+print_r($_3x3);
