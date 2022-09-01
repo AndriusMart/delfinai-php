25b69f5bbcbd534266aedb001f9c036f92fe3e78
Andrius Martinkėnas <andrius.martinkenas321@gmail.com>
2022-09-01 14:22:21 +0300

    react

--- 026/App/App.php
+++ 026/App/App.php
@@ -5,10 +5,12 @@ namespace App;
 use App\Controllers\HomeController as H;
 use App\Controllers\AnimalController as A;
 use App\Controllers\LoginController as L;
+use App\Controllers\ApiController as Api;
 use App\Middlewares\Auth;
 
 
-class App {
+class App
+{
 
 
     static public function start()
@@ -21,56 +23,61 @@ class App {
 
     static public function router()
     {
-                
+
         $url = $_SERVER['REQUEST_URI'];
-       
+
         $url = explode('/', $url);
         array_shift($url);
         $method = $_SERVER['REQUEST_METHOD'];
 
-        if(!Auth::authorize($url)){
+        if (!Auth::authorize($url)) {
             return self::redirect('login');
         }
 
 
         if ($method == 'GET' && count($url) == 1 && $url[0] == '') {
-            return((new H)->home());
+            return ((new H)->home());
         }
         if ($method == 'GET' && count($url) == 2 && $url[0] == 'animals' && $url[1] == 'create') {
-            return((new A)->create());
+            return ((new A)->create());
         }
         if ($method == 'POST' && count($url) == 2 && $url[0] == 'animals' && $url[1] == 'store') {
-            return((new A)->store());
+            return ((new A)->store());
         }
-        if ($method == 'GET' && count($url) == 1 && $url[0] == 'animals' ) {
-            return((new A)->list());
+        if ($method == 'GET' && count($url) == 1 && $url[0] == 'animals') {
+            return ((new A)->list());
         }
         if ($method == 'GET' && count($url) == 3 && $url[0] == 'animals' && $url[1] == 'edit') {
-            return((new A)->edit((int) $url[2])); 
+            return ((new A)->edit((int) $url[2]));
         }
         if ($method == 'POST' && count($url) == 3 && $url[0] == 'animals' && $url[1] == 'update') {
-            return((new A)->update((int) $url[2])); 
+            return ((new A)->update((int) $url[2]));
         }
         if ($method == 'POST' && count($url) == 3 && $url[0] == 'animals' && $url[1] == 'delete') {
-            return((new A)->delete((int) $url[2])); 
+            return ((new A)->delete((int) $url[2]));
         }
         if ($method == 'GET' && count($url) == 1 && $url[0] == 'login') {
-            if(Auth::isLoged()){
+            if (Auth::isLoged()) {
                 return self::redirect('');
             }
-            return((new L)->login()); 
+            return ((new L)->login());
         }
         if ($method == 'POST' && count($url) == 1 && $url[0] == 'login') {
-            return((new L)->doLogin());
+            return ((new L)->doLogin());
         }
 
         if ($method == 'POST' && count($url) == 1 && $url[0] == 'logout') {
-            return((new L)->logout());
+            return ((new L)->logout());
         }
-
+        if ($method == 'GET' && count($url) == 2 && $url[0] == 'api' && $url[1] == 'go') {
+            return ((new Api)->show());
+        }
+        if ($method == 'POST' && count($url) == 2 && $url[0] == 'api' && $url[1] == 'go') {
+            return ((new Api)->doApi());
+        }
     }
 
-    static public function view($name, $data= [])
+    static public function view($name, $data = [])
     {
         extract($data);
         require DIR . 'resources/view/' . $name . '.php';
@@ -80,5 +87,4 @@ class App {
     {
         header('Location: ' . URL . $where);
     }
-
 }
