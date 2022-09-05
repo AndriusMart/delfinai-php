53c42ddd6e294124ee778c595640270519df6e0d
Andrius Martinkėnas <andrius.martinkenas321@gmail.com>
2022-09-05 14:29:29 +0300

    030

---
+++ 026/App/Controllers/ReactController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controllers;
+
+use App\App;
+use App\DB\Json;
+
+class ReactController
+{
+
+    public function list()
+    {
+        return App::json(Json::connect()->showAll());
+    }
+
+    public function store()
+    {
+        $rawData = file_get_contents("php://input");
+        $rawData = json_decode($rawData, 1);
+        Json::connect()->create([
+            'type' => $rawData['type'],
+            'weight' => $rawData['weight'],
+            'tail' => $rawData['tail']
+        ]);
+        return App::json(['msg' => 'Hello, zoo']);
+    }
+
+    public function delete(int $id)
+    {
+        Json::connect()->delete($id);
+        return App::json(['msg' => 'Hello, zoo']);
+    }
+}
